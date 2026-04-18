c20af82f28c837cea760fd196a0c89e733fa500b
thiago monteiro <zoothiago@gmail.com>
2026-04-18 13:27:38 -0300

    banco de dados sql

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE treina_recife
+
+CREATE table tb_alunos (
+    id int  AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (50) NOT NULL,
+    email VARCHAR (150) NOT NULL,
+    data_nascimento  DATETIME NOT NULL
+
+)
+INSERT INTO treina_recife.tb_alunos
+("id", "nome", "emial", "data_nascimento")
+VALUS
+(1, "Alexandre de souza JR", "Alexandre@treinarecife.com.br", "20/12/1984")
